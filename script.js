ee761149b4167f817c85cd925fbffd696c249a6b
Levi <127esacramento@frhsd.com>
2025-12-12 12:52:20 -0500

    progress

---
+++ script.js
@@ -0,0 +1,13 @@
+//('#circleDrop').click(function(){
+  //'.card-middle').slideToggle();
+  //('.close').toggleClass('closeRotate');
+//});
+
+const downDrop=document.getElementById("circleDrop");
+console.log(downDrop);
+
+downDrop.addEventListener(click(function(){
+  $(".card-middle").slideToggle();
+  $(".close").toggleClass("closeRotate");
+
+}));
